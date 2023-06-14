5bf83746cf1abd5bfbbd711fad6badb4e7e47437
karalkal <karalkal@yahoo.co.uk>
2023-06-14 18:42:51 +0100

    added setTimeout and setInterval

---
+++ async-JS/setTimeout-setInterval.js
@@ -0,0 +1,35 @@
+function printOnceWithDelay() {
+    setTimeout(
+        () => console.log("... and after 4 seconds the callback function will be called asynchronously"),
+        4000);
+    console.log('Doing important stuff...');
+    console.log('...Still doing important stuff.');
+}
+
+
+function printEightTimesAfter4Seconds() {
+    // asign setInterval to var, so it can be stopped with clearInterval
+    let nIntervId = setInterval(
+        () => {
+            for (let pesho = 1; pesho <= 8; pesho++) {
+                console.log("Function called asynchronously", pesho);
+            }
+            console.log("without clearInterval(nIntervId) the loop will restart indefinitelly (not the same as indefinite loop though)")
+            clearInterval(nIntervId)
+        },
+        4000);
+
+    console.log('Doing important stuff...');
+    console.log('...Still doing important stuff.');
+}
+
+
+
+
+/* Uncomment for demo */
+// console.log("=== setTimeout ===")
+// printOnceWithDelay()
+
+
+console.log("=== setInterval ===")
+printEightTimesAfter4Seconds()
