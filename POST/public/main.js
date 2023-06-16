1ffb5b0e55248a9d065c7784687b9ef9b1edd044
karalkal <karalkal@yahoo.co.uk>
2023-06-16 22:18:54 +0100

    added POST with async await

--- POST/public/main.js
+++ POST/public/main.js
@@ -8,7 +8,8 @@ const inputField = document.querySelector("#input");
 const shortenButton = document.querySelector("#shorten");
 const responseField = document.querySelector("#responseField");
 
-// Asynchronous functions
+// Asynchronous functions with .then
+/*
 const shortenUrl = () => {
     const urlToShorten = inputField.value;
     const data = JSON.stringify({ destination: urlToShorten });
@@ -34,6 +35,34 @@ const shortenUrl = () => {
             renderResponse(jsonResponse);
         });
 };
+*/
+// Asynchronous functions with async / await
+const shortenUrl = async () => {
+    const urlToShorten = inputField.value;
+    const data = JSON.stringify({ destination: urlToShorten });
+    try { nhgdh
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+                apikey: apiKey,
+            },
+            body: data,
+        });
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            renderResponse(jsonResponse);
+            return      // [my code, just in case]
+        }       // if not ok [my code] 
+        throw new Error(`Request failed! with ${response.status} - ${response.statusText}`);
+
+    } catch (error) {
+        console.log(error);
+        // [my code] 
+        throw new Error(`Request failed! with ${error.name} - ${error.message}`);
+    }
+};
+
 
 // Clear page and call Asynchronous functions
 const displayShortUrl = (event) => {
