7743338fbf546dfd1dceed2045571364d2149889
karalkal <karalkal@yahoo.co.uk>
2023-08-25 20:30:23 +0300

    fixed styling of menu items, added two types of OAuth

--- 23-react--reddit/src/components/Header.js
+++ 23-react--reddit/src/components/Header.js
@@ -8,22 +8,33 @@ export default function Header({ authEndpoint, clientId, responseType, randomStr
         <img src='logo.svg' className={styles["logo"]} alt="logo" />
         <h1>Mini Reddit</h1>
         <nav>
-            <NavLink to="/">
+            <NavLink to={`${authEndpoint}?client_id=${clientId}&response_type=${responseType}&state=${randomStr}&redirect_uri=${redirectURI}&duration=${duration}&scope=${scopeStr}`}
+                className={styles["first-row-item"]}>
+                <Button                    >
+                    {!hasGrantedAccess ? "Grant" : "Remove"} User Auth
+                </Button>
+            </NavLink>
+
+            <NavLink to="kur"
+                className={styles["first-row-item"]}>
+                <Button>
+                    App Only OAuth (userless)
+                </Button>
+            </NavLink>
+
+            <NavLink to="/"
+                className={styles["second-row-item"]}>
                 <Button>Home</Button>
             </NavLink>
 
-            <NavLink to="random">
+            <NavLink to="random"
+                className={styles["second-row-item"]}>
                 <Button>Random</Button>
             </NavLink>
 
-            <NavLink to="search">
+            <NavLink to="search"
+                className={styles["second-row-item"]}>
                 <Button>Search</Button>
-            </NavLink>
-
-            <NavLink to={`${authEndpoint}?client_id=${clientId}&response_type=${responseType}&state=${randomStr}&redirect_uri=${redirectURI}&duration=${duration}&scope=${scopeStr}`}>
-                <Button>
-                    {!hasGrantedAccess ? "Grant" : "Remove"} Access
-                </Button>
             </NavLink>
         </nav>
     </header>
