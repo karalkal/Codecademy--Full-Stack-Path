23fdb53035d04fe4ee8da26ea37801d05ac0436d
karalkal <karalkal@yahoo.co.uk>
2023-08-28 01:14:16 +0100

    changed styling of header

--- 23-react--reddit/src/components/Header.js
+++ 23-react--reddit/src/components/Header.js
@@ -5,8 +5,10 @@ import { NavLink } from 'react-router-dom'
 
 export default function Header({ authEndpoint, clientId, responseType, randomStr, redirectURI, duration, scopeStr, hasGrantedAccess }) {
     return <header id={styles.header}>
-        <img src='logo.svg' className={styles["logo"]} alt="logo" />
-        <h1>Mini Reddit</h1>
+        <div>
+            <img src='logo.svg' className={styles["logo"]} alt="logo" />
+            <h1>Mini Reddit</h1>
+        </div>
         <nav>
             <NavLink to={`${authEndpoint}?client_id=${clientId}&response_type=${responseType}&state=${randomStr}&redirect_uri=${redirectURI}&duration=${duration}&scope=${scopeStr}`}
                 className={styles["first-row-item"]}>
