0e99a81f47f865d3d95226d1b25b0299089f3aac
karalkal <karalkal@yahoo.co.uk>
2023-08-29 21:27:50 +0100

    cards linked to content via permalink (reddit), not url (linked to external pages)

--- 23-react--reddit/src/components/Card.js
+++ 23-react--reddit/src/components/Card.js
@@ -2,22 +2,28 @@ import styles from "./Card.module.css"
 import { Link } from "react-router-dom"
 
 export default function Card({ result }) {
-    //text, author, icons, main_icon, upvotes, created_utc, media_metadata, secure_media
+    //text, author, icons, main_icon, upvotes, created_utc, media_metadata, secure_media, permalink
 
 
     // The Date constructor from Javascript accepts the number of milliseconds as timestamp, not unix time (number of seconds).
     // So, to adjust that, is just multiply the unix time by 1000.
     const unixTime = result.created_utc * 1000
-    console.log(result.secure_media, result.media_metadata)
+    const redditLink = "https://www.reddit.com" + result.permalink
+    console.log(result.title, "Permalink:", result.permalink, "URL:", result.url)
     const videoFile = (
-        <video width="750" height="500" controls className={`${styles.videoWindow} ${styles.mediaCardContent} `}>
+        <video muted controls className={styles.mediaCardContent} >
             <source src={result.secure_media} type="video/mp4" />
         </video>
     )
 
+    const imgFile = (
+        <img src="https://preview.redd.it/6j8wydgbm2lb1.png?auto=webp&amp;s=1e644bf1bbc32e8ea4a6aecb0fa9db9be4ba5f94"></img>
+    )
+
     return (
-        <Link to={result.url} className={styles.card}>
-            <div key={result.id} >
+        <Link to={redditLink} className={styles.card} key={result.id}>
+            <div >
+                {/* {imgFile} */}
 
                 {result.secure_media ? <h1>{videoFile}</h1> : ""}
                 <div className={styles.cardTitle}>{result.title}</div>
@@ -39,7 +45,7 @@ export default function Card({ result }) {
                     }
                 </div>
             </div>
-            
-            <Link to={result.url} className={styles.linkToPost}>Visit</Link>
+
+            <Link to={redditLink} className={styles.linkToPost}>View</Link>
         </Link>)
 }
