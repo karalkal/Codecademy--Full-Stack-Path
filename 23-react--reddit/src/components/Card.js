58217e32bb0e5681a2a9598c0fed09116885adc4
karalkal <karalkal@yahoo.co.uk>
2023-09-03 01:31:20 +0100

    reinstated visit btn

--- 23-react--reddit/src/components/Card.js
+++ 23-react--reddit/src/components/Card.js
@@ -25,78 +25,81 @@ export default function Card({ result }) {
 
 
     return (
-        <Link to={redditLink} className={styles.card} target="_blank" id={nanoid()}>
-            <div className={styles.textCardContent}>
-                {/* Icons and link*/}
-                <div className={styles.cardTopSection}>
-                    <div className={styles.cardIcons}>
-                        {result.main_icons.map((bdg, index) =>
-                            <img src={bdg.icon_url}
-                                alt={bdg.icon_name}
-                                title={`****    ${bdg.icon_name}    ****\n${bdg.icon_description}`}
-                                key={index}>
-                            </img>
-                        )}
+        <div className={styles.cardContainer}>
+            <div className={styles.card} id={nanoid()}>
+                <div className={styles.textCardContent}>
+                    {/* Icons and link*/}
+                    <div className={styles.cardTopSection}>
+                        <div className={styles.cardIcons}>
+                            {result.main_icons.map((bdg, index) =>
+                                <img src={bdg.icon_url}
+                                    alt={bdg.icon_name}
+                                    title={`****    ${bdg.icon_name}    ****\n${bdg.icon_description}`}
+                                    key={index}>
+                                </img>
+                            )}
+                        </div>
+                        {/* <div className={styles.linkToPost}>View</div> */}
                     </div>
-                    {/* <div className={styles.linkToPost}>View</div> */}
+
+                    {/* title, author, datestamp */}
+                    <div className={styles.cardTitle}>{htmlDecode(result.title)}</div>
+                    <div className={styles.cardAuthor}>
+                        <PiPencilLineLight />&nbsp;
+                        Posted&nbsp;by&nbsp;
+                        <span>{htmlDecode(result.author)} </span>
+                        on&nbsp;
+                        <span>{new Date(unixTime).toLocaleDateString()}</span>
+                    </div>
+                    {/* subreddit, text, rating*/}
+                    <div className={styles.cardSubreddit}>
+                        <IoLogoReddit />&nbsp;
+                        r/
+                        <span>{result.subreddit}</span>
+                    </div>
+                    <div className={styles.postRating}>
+                        <BsArrowDownUp />&nbsp;
+                        upvotes&nbsp;<span>{result.upvotes}</span>
+                        &nbsp;
+                        / &nbsp;ratio&nbsp;<span>{result.upvote_ratio}</span>
+                        / &nbsp;comments&nbsp;<span>{result.num_comments}</span>
+                    </div>
+                    <div className={styles.cardText}>
+                        {result.text.length > 440
+                            ? `${htmlDecode((result.text).substring(0, 440))}...`
+                            : `${htmlDecode(result.text)} `
+                        }
+                    </div>
+
                 </div>
 
-                {/* title, author, datestamp */}
-                <div className={styles.cardTitle}>{htmlDecode(result.title)}</div>
-                <div className={styles.cardAuthor}>
-                    <PiPencilLineLight />&nbsp;
-                    Posted&nbsp;by&nbsp;
-                    <span>{htmlDecode(result.author)} </span>
-                    on&nbsp;
-                    <span>{new Date(unixTime).toLocaleDateString()}</span>
-                </div>
-                {/* subreddit, text, rating*/}
-                <div className={styles.cardSubreddit}>
-                    <IoLogoReddit />&nbsp;
-                    r/
-                    <span>{result.subreddit}</span>
-                </div>
-                <div className={styles.postRating}>
-                    <BsArrowDownUp />&nbsp;
-                    upvotes&nbsp;<span>{result.upvotes}</span>
-                    &nbsp;
-                    / &nbsp;ratio&nbsp;<span>{result.upvote_ratio}</span>
-                    / &nbsp;comments&nbsp;<span>{result.num_comments}</span>
-                </div>
-                <div className={styles.cardText}>
-                    {result.text.length > 440
-                        ? `${htmlDecode((result.text).substring(0, 440))}...`
-                        : `${htmlDecode(result.text)} `
-                    }
-                </div>
-
+                {/* IMG if any */}
+                {imageSrc &&
+                    <img src={imageSrc}
+                        alt={`article titled ${result.title}`}
+                        className={styles.mediaCardContent}>
+                    </img>}
+                {/* VIDEO if any */}
+                {/* Reddit video */}
+                {result.video && result.video.videoProvider === "reddit" &&
+                    <video
+                        controls
+                        className={styles.mediaCardContent} >
+                        <source src={result.video.videoSrc} type="video/mp4" />
+                        <source src={result.video.videoSrc} type="video/ogg" />
+                        Your browser does not support the video tag.
+                    </video>
+                }
+                {/* Youtube iframe */}
+                {result.video && result.video.videoProvider === "youtube" &&
+                    <iframe title="gyz" className={styles.mediaCardContent} src={result.video.videoSrc}></iframe>
+                }
             </div>
 
-            {/* IMG if any */}
-            {imageSrc &&
-                <img src={imageSrc}
-                    alt={`article titled ${result.title}`}
-                    className={styles.mediaCardContent}>
-                </img>}
-            {/* VIDEO if any */}
-            {/* Reddit video */}
-            {result.video && result.video.videoProvider === "reddit" &&
-                <video
-                    controls
-                    className={styles.mediaCardContent} >
-                    <source src={result.video.videoSrc} type="video/mp4" />
-                    <source src={result.video.videoSrc} type="video/ogg" />
-                    Your browser does not support the video tag.
-                </video>
-            }
-            {/* Youtube iframe */}
-            {result.video && result.video.videoProvider === "youtube" &&
-                <iframe title="gyz" className={styles.mediaCardContent} src={result.video.videoSrc}></iframe>
-            }
-
-
-
-
-        </Link>)
+            {/* vitit link */}
+            <Link to={redditLink} target="_blank" className={styles.linkToPost}>
+                visit
+            </Link>
+        </div>)
 
 }
