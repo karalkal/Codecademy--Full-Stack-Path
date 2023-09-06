071bb0a46f90c2fde6d82e3be4e0a5fed33087d8
karalkal <karalkal@yahoo.co.uk>
2023-09-06 22:56:29 +0100

    improved styling, added home and about btns

--- 23-react--reddit/src/components/Header.js
+++ 23-react--reddit/src/components/Header.js
@@ -1,40 +1,38 @@
 import SearchBar from './SearchBar'
 import styles from './Header.module.css'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 
 export default function Header() {
     // console.log(props)
     return <header id={styles.header}>
         <NavLink to="/">
-                <img src='logo.svg' className={styles["logo"]} alt="logo" />
-                <h1>Mini Reddit</h1>
+            <img src='logo.svg' className={styles["logo"]} alt="logo" />
+            <h1>Mini Reddit</h1>
         </NavLink>
 
         <nav>
-            <div className={`${styles.firstRowItem}`}>
-                <SearchBar className={`${styles.firstRowItem}`} />
+            <p className={styles.menuAboutRow}>Results from all Subreddits</p>
+
+            <div className={`${styles.menuSearchRow}`}>
+                <SearchBar />
             </div>
 
-            <NavLink to="best"
-                className={`${styles.secondRowItem} ${styles.navLink}`}>
-                Best
-            </NavLink>
-
-            <NavLink to="top"
-                className={`${styles.secondRowItem} ${styles.navLink}`}>
-                Top
-            </NavLink>
-
-            <NavLink to="hot"
-                className={`${styles.secondRowItem} ${styles.navLink}`}>
-                Hot
-            </NavLink>
-
-            <NavLink to="controversial"
-                className={`${styles.secondRowItem} ${styles.navLink}`}>
-                Contro
-            </NavLink>
+            <div className={styles.menuButtonsRow}>
+                <NavLink to="/"
+                    className={styles.navLink}>Home</NavLink>
+                <NavLink to="best"
+                    className={styles.navLink}>Best</NavLink>
+                <NavLink to="top"
+                    className={styles.navLink}>Top</NavLink>
+                <NavLink to="hot"
+                    className={styles.navLink}>Hot</NavLink>
+                <NavLink to="controversial"
+                    className={styles.navLink}>Contro</NavLink>
+                <Link to={"https://github.com/karalkal/Codecademy-Front-End/tree/main/23-react--reddit"} className={styles.navLink} target='_blank' id={styles.aboutBtn}>
+                    About
+                </Link>
+            </div>
         </nav>
     </header>
 }
