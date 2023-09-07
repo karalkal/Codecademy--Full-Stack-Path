21c2e0b786843764a4d524b62890ce5dacfb170f
karalkal <karalkal@yahoo.co.uk>
2023-09-07 21:56:45 +0100

    fixed bug with videos from unsupported sources

--- 23-react--reddit/src/utils/getRelatedVideos.js
+++ 23-react--reddit/src/utils/getRelatedVideos.js
@@ -3,20 +3,19 @@ export default function getRelatedVideos(obj) {
     // hence the former need to be rendered as <video>, the latter as <iframe> 
     // the function will construct object accordingly, so that it could be utilized in <Card />
     // if neither, function will return null
-
     let video = null
     if (!obj.data.secure_media) {     // if no video in json return null
         return null
     }
-    else {
-        console.log(obj.data.secure_media.reddit_video)
+    else {      // if media exists
+        // if reddit video
         if (obj.data.secure_media.reddit_video
-            && obj.data.secure_media.reddit_video.fallback_url
-            && (obj.data.secure_media.reddit_video.fallback_url).includes("v.redd.it")) {       // if reddit video
+            && obj.data.secure_media.reddit_video.fallback_url) {
             const videoSrc = obj.data.secure_media.reddit_video.fallback_url
             video = { videoProvider: "reddit", videoSrc }
         }
-        else if (obj.data.secure_media.oembed.html) {   // check if from youtube
+        // if from youtube
+        else if (obj.data.secure_media.oembed.html) {
             /*
             it will look sometthing like:
             &lt;iframe width="356" height="200" src="https://www.youtube.com/embed/9RFMQEEmU8g?feature=oembed&amp;enablejsapi=1" frameborder="0" .....
@@ -28,13 +27,14 @@ export default function getRelatedVideos(obj) {
 
             const videoSrc = htmlOfJson.slice(indexOfSrcTag + 5, indexOfTrailingSemiColon)
 
-            if (videoSrc.startsWith('https://www.youtube.com'))
-                console.log(videoSrc)
-            video = { videoProvider: "youtube", videoSrc }
+            if (videoSrc.startsWith('https://www.youtube.com')) {
+                video = { videoProvider: "youtube", videoSrc }
+            }
         }
         else {
             return null
         }
     }
+
     return video
 }
