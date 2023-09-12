99855fbfb6c4e09c76578acff5cf5bbe90d9c151
karalkal <karalkal@yahoo.co.uk>
2023-09-12 21:58:18 +0100

    finished details page excl. comments

--- 23-react--reddit/src/pages/Details.js
+++ 23-react--reddit/src/pages/Details.js
@@ -4,7 +4,7 @@ import { useParams, Link } from "react-router-dom"
 import { decode as htmlDecode } from 'html-entities';    // deals with html entities which are not displayed properly in JSX
 
 import { PiPencilLineLight } from "react-icons/pi";
-import {TfiTime } from "react-icons/tfi";
+import { TfiTime } from "react-icons/tfi";
 import { PiArrowsDownUpLight } from "react-icons/pi";
 import { VscCommentDraft } from "react-icons/vsc";
 
@@ -40,17 +40,13 @@ function Details({ accessToken, setDynamicUrlPath }) {
     // So, to adjust that, is just multiply the unix time by 1000.
     const unixTime = postObj.created_utc * 1000
     let date = new Date(unixTime);
-    // Hours part from the timestamp
     let hours = date.getHours();
-    // Minutes part from the timestamp
     let minutes = "0" + date.getMinutes();
-    // Seconds part from the timestamp
     let seconds = "0" + date.getSeconds();
-    
-    // Will display time in 10:30:23 format
+
     let formattedTime = hours + 'h ' + minutes.substr(-2) + 'm ' + seconds.substr(-2) + "s";
     let formattedDate = date.toLocaleDateString()
-    
+
     // if thumbnail is "self" (no idea what this is) or we have video (video has thumbnail attached too)
     // check for extension too as articles with no img also have "url_overridden_by_dest"
     // check for video  as well
@@ -58,45 +54,103 @@ function Details({ accessToken, setDynamicUrlPath }) {
     if (postObj.img_thumbnail && !postObj.media
         && (postObj.img_thumbnail.slice(-4) === ".png"
             || postObj.img_thumbnail.slice(-4) === ".jpg"
-            || postObj.img_thumbnail.slice(-4) === ".gif")
-    ) {
+            || postObj.img_thumbnail.slice(-4) === ".gif")) {
         imageSrc = postObj.img_thumbnail
     }
 
 
     return (
         <main className={styles.mainContainer}>
-            <div className={styles.postTopSection}>
-                <h1 className={styles.postTitle}>{postObj.title}</h1>
-                {/* <img src={selectedSubReddit.icon} alt={selectedSubReddit.name} className={styles.galleryIcon}></img> */}
-                <h2 className={styles.postSubtitle}>r/{postObj.subreddit}</h2>
-            </div>
 
-            <div className={styles.postMidSection}>
-                <div>
-                    <PiPencilLineLight />&nbsp;
-                    posted&nbsp;by&nbsp;
-                    <span>{htmlDecode(postObj.author)} </span>                   
-                </div>
-                <div>
-                    <TfiTime />&nbsp;                    
-                    on&nbsp;
-                    <span>{formattedDate} </span>
-                    at <span>{formattedTime}</span>
-                </div>
-                {/* subreddit, text, rating*/}
-                <div>
-                    <PiArrowsDownUpLight />&nbsp;
-                    upvotes&nbsp;<span>{postObj.upvotes}</span>
-                    &nbsp;
-                    / &nbsp;ratio&nbsp;<span>{postObj.upvote_ratio}</span>
-                </div>
-                <div>
-                    <VscCommentDraft />&nbsp;
-                    comments&nbsp;<span>{postObj.num_comments}</span>
-                </div>
-            </div>
+            <div className={styles.innerHorizContainer}>
 
+                <div className={styles.textCardContent}>
+
+                    {postObj.main_icons &&
+                        <div className={styles.postIcons}>
+                            {postObj.main_icons.map((bdg, index) =>
+                                <img src={bdg.icon_url}
+                                    alt={bdg.icon_name}
+                                    title={`****    ${bdg.icon_name}    ****\n${bdg.icon_description}`}
+                                    key={index}>
+                                </img>
+                            )}
+                        </div>
+                    }
+
+                    <div className={styles.postTopSection}>
+                        <h1 className={styles.postTitle}>{postObj.title}</h1>
+                        {/* <img src={selectedSubReddit.icon} alt={selectedSubReddit.name} className={styles.galleryIcon}></img> */}
+                        <h2 className={styles.postSubtitle}>r/{postObj.subreddit}</h2>
+                    </div>
+
+                    <div className={styles.postMidSection}>
+                        <div>
+                            <PiPencilLineLight />&nbsp;
+                            posted&nbsp;by&nbsp;
+                            <span>{htmlDecode(postObj.author)} </span>
+                        </div>
+                        <div>
+                            <TfiTime />&nbsp;
+                            on&nbsp;
+                            <span>{formattedDate} </span>
+                            at <span>{formattedTime}</span>
+                        </div>
+                        {/* subreddit, text, rating*/}
+                        <div>
+                            <PiArrowsDownUpLight />&nbsp;
+                            upvotes&nbsp;<span>{postObj.upvotes}</span>
+                            &nbsp;
+                            / &nbsp;ratio&nbsp;<span>{postObj.upvote_ratio}</span>
+                        </div>
+                        <div>
+                            <VscCommentDraft />&nbsp;
+                            comments&nbsp;<span>{postObj.num_comments}</span>
+                        </div>
+                    </div>
+
+                    <div className={styles.postText}>
+                        {htmlDecode(postObj.text)}
+                    </div>
+
+                </div>
+
+                {/* MEDIA DIV IF ANY */}
+                {/* IMG if any */}
+                {imageSrc &&
+                    <img src={imageSrc}
+                        alt={`article titled ${postObj.title}`}
+                        className={styles.mediaCardContent}>
+                    </img>}
+                {/* VIDEO if any */}
+                {/* Reddit video */}
+                {postObj.video && postObj.video.videoProvider === "reddit" &&
+                    <video
+                        controls
+                        muted={false}
+                        className={styles.mediaCardContent} >
+                        <source src={postObj.video.videoSrc} type="video/mp4" />
+                        <source src={postObj.video.videoSrc} type="video/ogg" />
+                        Your browser does not support the video tag.
+                    </video>
+                }
+                {/* gifv video */}
+                {postObj.video && postObj.video.videoProvider === "gifv" &&
+                    <video
+                        controls
+                        muted={false}
+                        className={styles.mediaCardContent} >
+                        <source src={postObj.video.videoSrc} type="video/mp4" />
+                        <source src={postObj.video.videoSrc} type="video/ogg" />
+                        Your browser does not support the video tag.
+                    </video>
+                }
+                {/* Youtube iframe */}
+                {postObj.video && postObj.video.videoProvider === "youtube" &&
+                    <iframe title="youtubevideo" className={styles.mediaCardContent} src={postObj.video.videoSrc}></iframe>
+
+                }
+            </div>
 
             {/* visit link */}
             <Link to={redditLink} target="_blank" className={styles.linkToPost}>
